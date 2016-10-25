cff3cfbfdad14f7f1e00889347529a453f67b620
赵宏亚 <macbook@macbookdemacbook-pro.local>
2016-10-25 17:56:34 +0800

    提交一下，添加保存按钮

--- 定制相册部分/定制相册部分/各个页面添加/选择证照附件/ChooseLicenseAccessoryVC.m
+++ 定制相册部分/定制相册部分/各个页面添加/选择证照附件/ChooseLicenseAccessoryVC.m
@@ -61,9 +61,6 @@
     [self.collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier:@"reuserFooter"];
     
     
-    
-    
-    
 #pragma mark 初始化数据
     NSArray *imageUrlArray = @[@"http://img.ivsky.com/img/bizhi/slides/201511/11/december.jpg",@"http://h.hiphotos.baidu.com/image/pic/item/267f9e2f0708283890f56e02bb99a9014c08f128.jpg",@"http://a.hiphotos.baidu.com/image/pic/item/b219ebc4b74543a9fa0c4bc11c178a82b90114a3.jpg",@"http://c.hiphotos.baidu.com/image/pic/item/024f78f0f736afc33b1dbe65b119ebc4b7451298.jpg",@"http://d.hiphotos.baidu.com/image/pic/item/77094b36acaf2edd481ef6e78f1001e9380193d5.jpg"];
 
@@ -71,7 +68,7 @@
         
         CertificateCellModel *model = [[CertificateCellModel alloc] init];
         model.isAlbum = NO;
-        model.cellImageType = CertificateCellImageEmpty;
+        model.cellImageType = CertificateCellImageDeselect;
         model.imageUrl = [imageUrlArray objectAtIndex:i];
         [self.modelArray addObject:model];
     }
