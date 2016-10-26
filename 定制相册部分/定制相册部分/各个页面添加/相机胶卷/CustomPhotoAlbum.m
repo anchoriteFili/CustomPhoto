416f56259434a117667fcd1f407a8e3e3d0982a5
赵宏亚 <macbook@macbookdemacbook-pro.local>
2016-10-26 12:23:14 +0800

    添加相册获取部分

--- 定制相册部分/定制相册部分/各个页面添加/相机胶卷/CustomPhotoAlbum.m
+++ 定制相册部分/定制相册部分/各个页面添加/相机胶卷/CustomPhotoAlbum.m
@@ -8,6 +8,7 @@
 
 #import "CustomPhotoAlbum.h"
 #import "CertificateCollectionViewCell.h"
+#import <Photos/Photos.h>
 
 @interface CustomPhotoAlbum ()<UICollectionViewDataSource,UICollectionViewDelegate,CertificateCollectionViewCellDelegate>
 
@@ -69,7 +70,20 @@
         [self.modelArray addObject:model];
     }
     
+    [self getPhotos];
+    
 }
+
+#pragma mark 测试部分，获取图片部分
+- (void)getPhotos {
+    
+
+
+
+
+
+}
+
 
 #pragma mark ****************** collectionView代理部分 begin ******************
 - (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
@@ -210,10 +224,69 @@
 - (IBAction)previewButtonClick:(UIButton *)sender {
 }
 
-
-
 #pragma mark *************** 页面中自定义按钮点击事件 end ***************
 
+
+
+#pragma mark ************ 相册获取部分 begin ****************
+#pragma mark 获取所有的原图
+- (void)getOriginalImages {
+    // 获得所有的自定义相簿
+    PHFetchResult<PHAssetCollection *> *assetCollections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    // 遍历所有的自定义相簿
+    for (PHAssetCollection *assetCollection in assetCollections) {
+        [self enumerateAssetsInAssetCollection:assetCollection original:YES];
+    }
+    
+    // 获得相机胶卷
+    PHAssetCollection *cameraRoll = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary options:nil].lastObject;
+    // 遍历相机胶卷,获取大图
+    [self enumerateAssetsInAssetCollection:cameraRoll original:YES];
+}
+
+#pragma mark 获取所有的缩略图
+- (void)getThumbnailImages {
+    // 获得所有的自定义相簿
+    PHFetchResult<PHAssetCollection *> *assetCollections = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeAlbum subtype:PHAssetCollectionSubtypeAlbumRegular options:nil];
+    // 遍历所有的自定义相簿
+    for (PHAssetCollection *assetCollection in assetCollections) {
+        [self enumerateAssetsInAssetCollection:assetCollection original:NO];
+    }
+    // 获得相机胶卷
+    PHAssetCollection *cameraRoll = [PHAssetCollection fetchAssetCollectionsWithType:PHAssetCollectionTypeSmartAlbum subtype:PHAssetCollectionSubtypeSmartAlbumUserLibrary options:nil].lastObject;
+    [self enumerateAssetsInAssetCollection:cameraRoll original:NO];
+}
+
+/**
+ *  遍历相簿中的所有图片
+ *  @param assetCollection 相簿
+ *  @param original        是否要原图
+ */
+- (void)enumerateAssetsInAssetCollection:(PHAssetCollection *)assetCollection original:(BOOL)original
+{
+    NSLog(@"相簿名:%@", assetCollection.localizedTitle);
+    
+    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+    // 同步获得图片, 只会返回1张图片
+    options.synchronous = YES;
+    
+    // 获得某个相簿中的所有PHAsset对象
+    PHFetchResult<PHAsset *> *assets = [PHAsset fetchAssetsInAssetCollection:assetCollection options:nil];
+    
+    NSLog(@"assets.count ========== %lu",(unsigned long)assets.count);
+    
+    for (PHAsset *asset in assets) {
+        // 是否要原图
+        CGSize size = original ? CGSizeMake(asset.pixelWidth, asset.pixelHeight) : CGSizeZero;
+        
+        // 从asset中获得图片
+        [[PHImageManager defaultManager] requestImageForAsset:asset targetSize:size contentMode:PHImageContentModeDefault options:options resultHandler:^(UIImage * _Nullable result, NSDictionary * _Nullable info) {
+            NSLog(@"%@", result);
+        }];
+    }
+}
+
+#pragma mark ************ 相册获取部分 end ****************
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
