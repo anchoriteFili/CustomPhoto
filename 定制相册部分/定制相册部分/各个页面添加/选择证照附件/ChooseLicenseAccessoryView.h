cb7a24f1ad5fb9d1962785e028d2f387183650d3
赵宏亚 <macbook@macbookdemacbook-pro.local>
2016-10-27 20:07:41 +0800

    添加预览页

---
+++ 定制相册部分/定制相册部分/各个页面添加/选择证照附件/ChooseLicenseAccessoryView.h
@@ -0,0 +1,13 @@
+//
+//  ChooseLicenseAccessoryView.h
+//  定制相册部分
+//
+//  Created by 赵宏亚 on 16/10/27.
+//  Copyright © 2016年 赵宏亚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChooseLicenseAccessoryView : UIView
+
+@end
---
+++ 定制相册部分/定制相册部分/各个页面添加/选择证照附件/ChooseLicenseAccessoryView.m
@@ -0,0 +1,21 @@
+//
+//  ChooseLicenseAccessoryView.m
+//  定制相册部分
+//
+//  Created by 赵宏亚 on 16/10/27.
+//  Copyright © 2016年 赵宏亚. All rights reserved.
+//
+
+#import "ChooseLicenseAccessoryView.h"
+
+@implementation ChooseLicenseAccessoryView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
